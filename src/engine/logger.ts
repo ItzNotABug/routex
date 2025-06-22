c9837e2fc7dd479410cfbabb2588b8b4048eb7a5
Darshan <itznotabug@gmail.com>
2025-06-22 11:40:04 +0530

    add: line breaks.

--- src/engine/logger.ts
+++ src/engine/logger.ts
@@ -4,15 +4,19 @@
 export class Logger {
     private static readonly PREFIX = '\x1b[2m\x1b[36m[redirects]\x1b[0m';
 
-    static log(message: string): void {
-        console.log(`\n${Logger.PREFIX} ${message}`);
+    static log(message: string, spaced = false): void {
+        console.log(`${spaced ? '\n' : ''}${Logger.PREFIX} ${message}`);
     }
 
-    static warn(message: string): void {
-        console.warn(`\n${Logger.PREFIX} \x1b[33m⚠\x1b[0m ${message}`);
+    static warn(message: string, spaced = false): void {
+        console.warn(
+            `${spaced ? '\n' : ''}${Logger.PREFIX} \x1b[33m⚠\x1b[0m ${message}`,
+        );
     }
 
-    static error(message: string): void {
-        console.error(`\n${Logger.PREFIX} \x1b[31m✖\x1b[0m ${message}`);
+    static error(message: string, spaced = false): void {
+        console.error(
+            `${spaced ? '\n' : ''}${Logger.PREFIX} \x1b[31m✖\x1b[0m ${message}`,
+        );
     }
 }
